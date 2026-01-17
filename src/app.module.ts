e2df112ad6e5861f0f59e7d1bb0685cb91a49ff9
Ciel <claudiaalca78@gmail.com>
2026-01-17 10:55:04 -0300

    implement docker persistence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+/*
 @Module({
   imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+*/
+@Module({
+  imports: [
+    // TypeOrmModule.forRoot() connetcion GLOBAL
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', //PC local.
+      port: 5432,
+      username: 'adminClau',      
+      password: 'adminClau', 
+      database: 'tp_academy_db', 
+      
+      // AUTO-LOAD ENTITIES:
+      // Esto busca automáticamente cualquier archivo que termine en .entity.ts
+      // y lo registra como tabla. En Java tendrías que listarlas en el persistence.xml.
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      
+      // SYNCHRONIZE: ¡OJO CON ESTO!
+      // true = Si cambias una clase, TypeORM altera la tabla real (DROP/ALTER).
+      // Es genial para desarrollo (dev), pero PROHIBIDO en producción (prod) porque borra datos.
+      // Equivalente a: hibernate.hbm2ddl.auto = update
+      synchronize: true, 
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user.service.ts
+++ src/user.service.ts
@@ -1,1 +1,23 @@
 //Activity 5. Add a comment to a user.service.ts file 
+
+
+
+
+
+//separate activity
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  
+  @PrimaryGeneratedColumn() 
+  id: number;
+
+  @Column() 
+  name: string;
+
+  @Column({ unique: true }) // UNIQUE in the db
+  email: string;
+
+  isActive: boolean;
+}
