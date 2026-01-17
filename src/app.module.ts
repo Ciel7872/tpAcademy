75fc896b1b25989b6c2ea3e6dca13c177b754b66
Ciel <claudiaalca78@gmail.com>
2026-01-17 20:45:27 -0300

    add insertion of user files both externally and internally

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { UserService } from './user.service';
       entities: [User],
 
       synchronize: true, 
+      dropSchema: true,
     }),
 
     TypeOrmModule.forFeature([User]),
--- src/user.service.ts
+++ src/user.service.ts
@@ -3,11 +3,13 @@
 
 
 //------
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { NotFoundException } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class UserService {
@@ -28,23 +30,20 @@ export class UserService {
   }
 
   //GET ONE 
-  async findOne(id: number): Promise<User> {
+  async findOne(id: number): Promise<User> {//Promise<User|null>
   const user = await this.usersRepository.findOneBy({ id });
   if (!user) {
-    throw new NotFoundException("Usuario: ID ${id} no encontrado");// Error 404
+    throw new NotFoundException('Usuario: ID ${id} no encontrado');// Error 404
   }
   return user;
   }
-  //findOne(id: number): Promise<User> {//Promise<User|null>
-    //return this.usersRepository.findOneBy({ id });
-  //}
-
+ 
   //UPDATE M
   async update(id: number, user: Partial<User>): Promise<User> {
     await this.usersRepository.update(id, user);//run the update
     const updatedUser = await this.usersRepository.findOneBy({ id });
     if (!updatedUser) {
-    throw new NotFoundException("Usuario con ID ${id} no encontrado.");
+    throw new NotFoundException('Usuario con ID ${id} no encontrado.');
   }return updatedUser
   }
 
@@ -52,9 +51,77 @@ export class UserService {
   async remove(id: number): Promise<void> {
     const res = await this.usersRepository.delete(id);
     if (res.affected ===0){
-      throw new NotFoundException("Usuario no encontrado con ID ${id}. ")
+      throw new NotFoundException('Usuario no encontrado con ID ${id}. ')
     }
 
   }
+
+//external manual loading---------------
+  async loadUsersFromFile(file: Express.Multer.File) {
+    const fileContent = file.buffer.toString('utf-8'); //buffer to text
+    let usersToInsert: Partial<User>[]=[]
+
+    if (file.mimetype === 'application/json' || file.originalname.endsWith('.json')) {//Detect the file type
+      usersToInsert = JSON.parse(fileContent);      //  JSON
+      
+    } else if (file.mimetype === 'text/csv' || file.originalname.endsWith('.csv')) {
+      // CSV   nombre,email,password 
+      const lines = fileContent.split('\n'); 
+      const dataLines = lines.slice(1); //head
+
+      usersToInsert = dataLines
+        .filter(line => line.trim() !== '') 
+        .map(line => {
+          const [firstName, lastName, email] = line.split(',');
+          return {//mapear to objet user
+            firstName: firstName.trim(),
+            lastName: lastName.trim(),
+            email: email.trim(),
+          
+          };
+        });
+    } else {
+      throw new BadRequestException('Formato no soportado. Use JSON o CSV');
+    }
+
+    const result = await this.usersRepository.save(usersToInsert);
+    return {
+      message: 'Usuarios cargados exitosamente',
+      count: result.length,
+    };
+
+  }
+
+  //interior load---------------
+
+  async onModuleInit() {
+    // init app
+    await this.insertUsersFromSeed();
+  }
+  async insertUsersFromSeed() {
+    try {
+      //It only works if the db is empty. enable dropSchema: true (located in appmodule)
+      const count = await this.usersRepository.count();
+      if (count > 0) {
+        console.log('La base de datos ya tiene usuarios. Saltando carga inicial (Seed).');
+        return;
+      }
+      const filePath = path.join(process.cwd(), 'users.json');
+      
+      if (!fs.existsSync(filePath)) {
+        console.log('No se encontró el archivo usuarios.json para la carga inicial.');
+        return;
+      }
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      const users: Partial<User>[] = JSON.parse(fileContent);
+
+      await this.usersRepository.save(users);
+      console.log(`Carga exitosa de datos: ${users.length} usuarios insertados automáticamente.`);
+
+    } catch (error) {
+      console.error('Error en el Seed:', error.message);
+    }
+  }
+
 
 }
